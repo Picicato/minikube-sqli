c23d2f30ca4cc880b8bbd3e037b88c7b017cbeb8
Picicato <45697137+Picicato@users.noreply.github.com>
2025-12-02 14:11:53 +0100

    update

--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@ set -e
 
 minikube start --container-runtime=docker --driver=docker
 
-# Build dans ton Docker local
+# Build docker images
 docker build -t vuln-app:latest ./vuln-web
 docker build -t sqli-detector:latest ./sqli-detector
 
 cd terraform
 
+# Initialize and apply Terraform configurations
+terraform init
 terraform apply -target=module.namespace \
                -target=module.postgres \
                -target=module.app \
@@ -18,5 +20,7 @@ terraform apply -target=module.namespace \
 
 terraform apply -target=module.monitoring_addons --auto-approve
 
+# Port forwarding
 minikube kubectl -- -n sqli-lab port-forward svc/vuln-app 5000:80 &
+minikube kubectl -- -n sqli-lab port-forward svc/kube-prometheus-stack-grafana 3000:80 &
 minikube kubectl -- -n sqli-lab port-forward svc/kube-prometheus-stack-prometheus 9090:9090 &
