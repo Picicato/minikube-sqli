d7857f4c1bf343f72230f7a60e4fb7029facbe63
Picicato <45697137+Picicato@users.noreply.github.com>
2025-12-02 13:38:33 +0100

    project update

--- webhook/test.sh
+++
@@ -1,4 +0,0 @@
-minikube kubectl -- -n monitoring port-forward svc/alert-api-svc 8080:80 &
-minikube kubectl -- -n monitoring port-forward svc/kube-prom-stack-kube-prome-prometheus 9090:9090 &
-minikube kubectl -- -n monitoring port-forward svc/kube-prom-stack-grafana 3000:80 &
-minikube kubectl -- -n monitoring port-forward svc/kube-prom-stack-kube-prome-alertmanager 9093:9093 &
